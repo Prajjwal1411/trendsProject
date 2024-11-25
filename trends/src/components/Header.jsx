f179af3a9d28ebd77fa4786d5a807a90f2d6c06d
Prajjwal Shukla <prajjwal14112000@gmail.com>
2024-11-25 15:48:24 -0500

    Add code to hamburger icon

--- trends/src/components/Header.jsx
+++ trends/src/components/Header.jsx
@@ -2,18 +2,37 @@
 
 
 
-import { Box, Flex, Image, Spacer, Button, IconButton, HStack, Link } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { 
+  Box, 
+  Flex, 
+  Image, 
+  IconButton, 
+  Link, 
+  HStack, 
+  Button, 
+  Drawer, 
+  DrawerBody, 
+  DrawerOverlay, 
+  DrawerContent, 
+  DrawerCloseButton, 
+  VStack, 
+  Spacer 
+} from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { useLocation } from "react-router-dom";
 import logo from "../media/logo.svg";
 
 export default function Header() {
-  const location = useLocation(); 
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
     <Box as="header" px={6} py={4} background="black" borderBottom="1px solid" borderColor="gray.700">
       <Flex alignItems="center">
-        {/* Logo Section */}
+
         <Link href="/" _hover={{ textDecoration: "none" }}>
           <Image 
             src={logo}
@@ -52,7 +71,7 @@ export default function Header() {
           </Link>
           <Link
             href="/about-us"
-            fontWeight={location.pathname === "/about-us" ? "bold" : "normal"} 
+            fontWeight={location.pathname === "/about-us" ? "bold" : "normal"}
             color="white"
             _hover={{ textDecoration: "none" }}
           >
@@ -78,8 +97,64 @@ export default function Header() {
           display={{ base: "flex", md: "none" }}
           variant="ghost"
           color="white"
+          onClick={toggleMenu}
         />
       </Flex>
+
+
+      <Drawer isOpen={isMenuOpen} placement="right" onClose={toggleMenu}>
+        <DrawerOverlay />
+        <DrawerContent bg="black">
+          <DrawerCloseButton color="white" />
+          <DrawerBody>
+            <VStack align="start" spacing={6} mt={4}>
+              <Link
+                href="/for-business"
+                fontWeight={location.pathname === "/for-business" ? "bold" : "normal"}
+                color="white"
+                _hover={{ textDecoration: "none" }}
+              >
+                For Business
+              </Link>
+              <Link
+                href="/blog"
+                fontWeight={location.pathname === "/blog" ? "bold" : "normal"}
+                color="white"
+                _hover={{ textDecoration: "none" }}
+              >
+                Blog
+              </Link>
+              <Link
+                href="/affiliate"
+                fontWeight={location.pathname === "/affiliate" ? "bold" : "normal"}
+                color="white"
+                _hover={{ textDecoration: "none" }}
+              >
+                Affiliate Program
+              </Link>
+              <Link
+                href="/about-us"
+                fontWeight={location.pathname === "/about-us" ? "bold" : "normal"}
+                color="white"
+                _hover={{ textDecoration: "none" }}
+              >
+                About Us
+              </Link>
+              <Link
+                href="/faq"
+                fontWeight={location.pathname === "/faq" ? "bold" : "normal"}
+                color="white"
+                _hover={{ textDecoration: "none" }}
+              >
+                FAQs
+              </Link>
+              <Button size="sm" variant="outline" color="white" borderColor="white" w="100%">
+                Log in
+              </Button>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Box>
   );
 }
